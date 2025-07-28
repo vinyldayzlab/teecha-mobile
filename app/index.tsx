50cbc9ef3b6c5486b0763d0d6f7cf07792105d91
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-07-28 17:48:48 -0300

    Adds tailwind config to project

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello World</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-cyan-950">Hello World</Text>
     </View>
   );
 }
