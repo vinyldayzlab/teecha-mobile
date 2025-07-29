1c842614d06d8e1f33a29183f970858780836d0b
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-07-29 15:57:44 -0300

    feature: creates custom button component

--- app/onboarding/account-verify.tsx
+++ app/onboarding/account-verify.tsx
@@ -1,10 +1,14 @@
-import { Link } from "expo-router";
+import { Link, useLocalSearchParams } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function AccountVerify() {
+  const { role } = useLocalSearchParams<{ role?: string }>();
+
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-primary text-5xl">Do you have an account?</Text>
+      <Text className="text-primary text-5xl">
+        Do you have an account? {role ?? "guest"}
+      </Text>
       <Link href="/signup">I don't have an account</Link>
       <Link href="/login">I'm already signed up</Link>
     </View>
