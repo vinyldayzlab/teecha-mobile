9207c695a6ae6c4dbc46ba1e900cca58c9d0d566
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-07-29 16:25:53 -0300

    feature: adds placeholder headings

--- app/onboarding/index.tsx
+++ app/onboarding/index.tsx
@@ -4,7 +4,7 @@ import { Text, View } from "react-native";
 export default function Onboarding() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-primary text-5xl">Teecha</Text>
+      <Text className="h1">Teecha</Text>
       <Button
         title="I'm a student"
         type="primary"
@@ -21,8 +21,6 @@ export default function Onboarding() {
           params: { role: "teacher" },
         }}
       />
-      <Text className="text-lg font-barlow">This is Barlow Regular</Text>
-      <Text className="text-lg font-barlow-bold">This is Barlow Bold</Text>
     </View>
   );
 }
