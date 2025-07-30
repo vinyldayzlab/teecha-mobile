d12c5672d0ed9a823d7211949580dcac79f4252a
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-07-30 17:01:52 -0300

    feature: adds navigation back to account verify

--- app/onboarding/account-verify.tsx
+++ app/onboarding/account-verify.tsx
@@ -1,16 +1,45 @@
+import Button from "components/Button";
+import OnboardingHeading from "components/OnboardingHeading";
+import { spacingVariants } from "constants/spacing";
+import { LinearGradient } from "expo-linear-gradient";
 import { Link, useLocalSearchParams } from "expo-router";
-import { Text, View } from "react-native";
+import { ScrollView, View } from "react-native";
 
 export default function AccountVerify() {
   const { role } = useLocalSearchParams<{ role?: string }>();
 
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-primary text-5xl">
-        Do you have an account? {role ?? "guest"}
-      </Text>
-      <Link href="/signup">I don't have an account</Link>
-      <Link href="/login">I'm already signed up</Link>
-    </View>
+    <ScrollView
+      contentContainerStyle={{
+        flexGrow: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: spacingVariants.viewPadding,
+        backgroundColor: "#F8FAFB",
+      }}
+      keyboardShouldPersistTaps="handled"
+    >
+      <View className="flex-1 flex-col gap-8 w-full items-center">
+        <OnboardingHeading title="Do you already have an account?" />
+        <View className="w-full flex flex-col gap-6">
+          <Button
+            title="No, create an account"
+            type="primary"
+            route={{
+              link: "/signup",
+              params: { role: role },
+            }}
+          />
+          <Button
+            title="Yes, login"
+            type="secondary"
+            route={{
+              link: "/login",
+              params: { role: role },
+            }}
+          />
+        </View>
+      </View>
+    </ScrollView>
   );
 }
---
+++ components/OnboardingHeading.tsx
@@ -0,0 +1,48 @@
+import { useRouter } from "expo-router";
+import { TouchableOpacityProps, View } from "react-native";
+import IconButton from "./IconButton";
+import { textVariants } from "constants/typography";
+import { Text } from "react-native";
+import Stepper from "./Stepper";
+import ArrowLeftIcon from "../assets/icons/ArrowLeftIcon";
+
+type OnboardingHeadingProps = {
+  title: string;
+  stepper?: boolean;
+  onPress?: () => void;
+} & TouchableOpacityProps;
+
+const OnboardingHeading: React.FC<OnboardingHeadingProps> = ({
+  title,
+  stepper = false,
+  onPress,
+}) => {
+  const router = useRouter();
+
+  return (
+    <View className="flex flex-col gap-4">
+      <View className="flex flex-row gap-2">
+        <IconButton
+          icon={<ArrowLeftIcon />}
+          type="secondary"
+          onPress={onPress ?? (() => router.back())}
+        />
+        {/* progress bar */}
+        {stepper && (
+          <Stepper
+            steps={[
+              { completed: true },
+              { current: true },
+              { current: false },
+              { current: false },
+            ]}
+          />
+        )}
+      </View>
+      {/* <Text className={`${textVariants.h1} text-neutral-`}>{title}</Text> */}
+      <Text className={`${textVariants.h2} text-neutral-900`}>{title}</Text>
+    </View>
+  );
+};
+
+export default OnboardingHeading;
