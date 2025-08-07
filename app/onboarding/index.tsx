738df3b299ceb7e0c45e84451d680e7b044d5ecb
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-08-07 19:08:32 -0300

    styling: updates onboarding

--- app/onboarding/index.tsx
+++ app/onboarding/index.tsx
@@ -1,35 +1,43 @@
 import ButtonGroup from "@components/buttons/ButtonGroup";
 import { textVariants } from "@constants/typography";
-import { Image, ScrollView, Text, View } from "react-native";
+import { Image, ImageBackground, ScrollView, Text, View } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import logo from "@assets/images/favicon.png";
 import { spacingVariants } from "@constants/spacing";
+import bgImage from "@assets/images/onboarding-bg-3.png";
+import icon from "@assets/images/icon.png";
 
 export default function Onboarding() {
   return (
-    <LinearGradient
-      colors={["#f0fbf8", "#ffffff", "#ffffff"]}
-      style={{ flex: 1, padding: 0 }}
-      start={{ x: 0, y: 0 }}
-      end={{ x: 0, y: 1 }}
-      className="flex-1"
-    >
+    <View className="flex-1 bg-neutral-800">
       <ScrollView
         contentContainerStyle={{
           flexGrow: 1,
-          justifyContent: "center",
+          justifyContent: "space-between",
           alignItems: "center",
-          padding: spacingVariants.viewPadding,
         }}
         keyboardShouldPersistTaps="handled"
+        showsVerticalScrollIndicator={false}
+        bounces={false}
       >
-        <View className="flex-1 w-full justify-center items-center">
-          <View className="flex-1 w-full h-1/3 justify-center items-center">
-            <Image source={logo} />
-          </View>
+        <Image
+          source={bgImage}
+          resizeMode="contain"
+          className="flex-1 w-full h-full px-12 aspect-square"
+        />
+
+        <View
+          style={{ padding: spacingVariants.viewPadding }}
+          className="w-full h-1/2 max-h-1/2 justify-center items-center rounded-t-[4rem] bg-neutral-300"
+        >
           <View className="flex flex-col gap-3 items-center mb-8">
+            {/*<Image
+              source={icon}
+              resizeMode="contain"
+              className="flex-1 w-16 h-16 aspect-square"
+            />*/}
             <Text className={`${textVariants.h1} text-center text-neutral-900`}>
-              Welcome to Teecha!
+              Welcome!
             </Text>
             <Text
               className={`${textVariants.body1} text-center text-neutral-700`}
@@ -60,6 +68,6 @@ export default function Onboarding() {
           />
         </View>
       </ScrollView>
-    </LinearGradient>
+    </View>
   );
 }
