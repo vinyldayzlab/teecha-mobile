77299d6f216efc2ef6e73b3bd12579301f91552b
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-08-08 15:21:58 -0300

    styling: adds teecha icon to home screen

---
+++ app/(auth)/index.tsx
@@ -0,0 +1,75 @@
+import ButtonGroup from "@components/buttons/ButtonGroup";
+import { textVariants } from "@constants/typography";
+import { Image, ScrollView, Text, View } from "react-native";
+import { spacingVariants } from "@constants/spacing";
+import bgImage from "@assets/images/teecha-white-text.png";
+import icon from "@assets/images/teecha-icon.png";
+import Icon from "@assets/icons/teecha-icon.svg";
+
+export default function Onboarding() {
+  return (
+    <View>
+      <ScrollView
+        contentContainerStyle={{
+          flexGrow: 1,
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+        keyboardShouldPersistTaps="handled"
+        showsVerticalScrollIndicator={false}
+        bounces={false}
+      >
+        <Image
+          source={bgImage}
+          resizeMode="contain"
+          className="flex-1 h-full w-full px-12 pt-16 aspect-square"
+        />
+
+        <View
+          style={{ padding: spacingVariants.viewPadding }}
+          className="relative w-full h-1/2 max-h-1/2 justify-center items-center rounded-t-[4rem] bg-neutral-300"
+        >
+          <View className="absolute top-0 -translate-y-1/2 w-44 h-44 shadow-md">
+            <Icon width="100%" height="100%" />
+          </View>
+          {/*<Image
+            source={icon}
+            resizeMode="contain"
+            className="absolute top-0 -translate-y-1/2 w-40 h-40 aspect-square"
+          />*/}
+          <View className="flex flex-col gap-3 items-center my-8">
+            <Text className={`${textVariants.h1} text-center text-neutral-900`}>
+              Welcome!
+            </Text>
+            <Text
+              className={`${textVariants.body1} text-center text-neutral-700`}
+            >
+              Manage your private classes as a teacher or access lessons as a
+              student.
+            </Text>
+          </View>
+          <ButtonGroup
+            buttons={[
+              {
+                title: "I'm a student",
+                type: "primary",
+                route: {
+                  link: "(auth)/account-verify",
+                  params: { role: "student" },
+                },
+              },
+              {
+                title: "I'm a teacher",
+                type: "secondary",
+                route: {
+                  link: "(auth)/account-verify",
+                  params: { role: "teacher" },
+                },
+              },
+            ]}
+          />
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
