1797d61aeccd70a611bb0dcf33998d3463866d71
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-08-09 17:45:02 -0300

    styling: updates topheader

--- components/layout/ContentView.tsx
+++ components/layout/ContentView.tsx
@@ -1,7 +1,13 @@
 import { View } from "react-native";
+import TopHeader from "@components/layout/TopHeader";
 
 const ContentView = ({ children }: { children: React.ReactNode }) => {
-  return <View className="bg-neutral-300 h-full">{children}</View>;
+  return (
+    <View className="bg-neutral-300 h-full">
+      <TopHeader />
+      {children}
+    </View>
+  );
 };
 
 export default ContentView;
