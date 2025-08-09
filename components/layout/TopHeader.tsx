ebc0305b1eb3968aad0410068f05193f5047f39c
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-08-09 17:45:02 -0300

    styling: updates topheader

---
+++ components/layout/TopHeader.tsx
@@ -0,0 +1,16 @@
+import { View, Image } from "react-native";
+import bgImage from "@assets/icons/teecha-black-text.png";
+import NotificationIcon from "@components/icons/NotificationIcon";
+
+const TopHeader = () => {
+  return (
+    <View className="flex flex-row items-center justify-between h-12 w-full pr-3">
+      <Image source={bgImage} resizeMode="contain" className="h-12 w-32" />
+      <View>
+        <NotificationIcon />
+      </View>
+    </View>
+  );
+};
+
+export default TopHeader;
